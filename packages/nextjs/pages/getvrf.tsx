5931558c2a05191ce1e2a366198779c9762aca86
Adam Jackson <18746720+atj3097@users.noreply.github.com>
2023-12-30 12:29:48 -0600

    Fixing the order of operations

--- packages/nextjs/pages/getvrf.tsx
+++ packages/nextjs/pages/getvrf.tsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from "react";
+import ambAbi from "../abi/Amb.json";
+import ambHelperAbi from "../abi/AmbHelper.json";
 import vrfConsumerAbi from "../abi/VRFv2DirectFundingConsumer.json";
 import yahoAbi from "../abi/Yaho.json";
 import yaruAbi from "../abi/Yaru.json";
-import { ethers, getAddress } from "ethers";
+import { AbiCoder, ethers, getAddress } from "ethers";
 import { MetaHeader } from "~~/components/MetaHeader";
 
-// Yaho(Chiado), Yaru(Chiado), and Chainlink VRF(Goerli) contracts
+// Yaho(Chiado), Yaru(Goerli), and Chainlink VRF(Goerli) contracts
 const yahoAddress = getAddress("0x0e729b11661B3f1C1E829AAdF764D5C3295e1256");
-const yaruAddress = getAddress("0x2EFf4B88Cfa00180409A5cF2f248CBF6Af4e22D6");
+const yaruAddress = getAddress("0x2EFf4B88Cfa00180409A5cF2f248CBF6Af4e22D6"); // needs to be goerli version still is chiado
 const vrfConsumerAddress = getAddress("0xa340bb99457D4E676824da9F20bD9df1684B1529");
 // Arbitrary Message Bridge Addresses
 const chiadoAmbAdapterAddress = getAddress("0x02EF808c1235EC235BdfEf9b5768527D86093711");
+const ambHelperAddress = getAddress("0xEd0dC0AA8A61c3Ac912072f50c4c5bd830d79E36");
 const goerliAmbAddress = getAddress("0x87A19d769D875964E9Cd41dDBfc397B2543764E6");
 
 const goerliProvider = new ethers.JsonRpcProvider(
@@ -19,7 +22,10 @@ const goerliProvider = new ethers.JsonRpcProvider(
 const chiadoProvider = new ethers.JsonRpcProvider(
   "https://rpc.ankr.com/gnosis_testnet/1b9d82f3f0d792f0cf027af1dc0abfd2d1f4d45e8ef23e5997375a67f3b384dc",
 );
-const chiadoWallet = new ethers.Wallet("", chiadoProvider);
+const chiadoWallet = new ethers.Wallet(
+  "494082f501c5289288401d48770a7ff35372d974e8d6dc36fc47c83fc5533106",
+  chiadoProvider,
+);
 
 const GetVRF = () => {
   const [vrf, setVrf] = useState("");
@@ -51,73 +57,75 @@ const GetVRF = () => {
   };
 
   const getVRF = async () => {
-    // Log the initiation of the VRF request process
     console.log("Starting the process to get VRF...");
-    setLoading(true); // Set loading state to true to indicate the process has started
+    setLoading(true);
 
     try {
-      // Get the signer object from the provider to sign transactions
-      // const signer = await chiadoProvider.getSigner();
-      // console.log('Signer obtained:', signer);
       const signer = chiadoWallet;
-      // Instantiate the Yaho contract with the signer to interact with it
       const yahoContract = new ethers.Contract(yahoAddress, yahoAbi.abi, signer);
-      console.log("Yaho contract instantiated:", yahoContract);
-
-      // Instantiate the Yaru contract with the signer to interact with it
       const yaruContract = new ethers.Contract(yaruAddress, yaruAbi.abi, signer);
-      console.log("Yaru contract instantiated:", yaruContract);
+      const vrfConsumerContract = new ethers.Contract(vrfConsumerAddress, vrfConsumerAbi.abi, goerliProvider);
+      const ambHelperContract = new ethers.Contract(ambHelperAddress, ambHelperAbi, goerliProvider);
+      const ambContractOnGoerli = new ethers.Contract(goerliAmbAddress, ambAbi, goerliProvider);
 
-      // Prepare the message to request a VRF
       const message = {
-        toChainId: ethers.getBigInt(5),
+        toChainId: ethers.toBigInt(5),
         to: vrfConsumerAddress,
         data: vrfConsumerContract.interface.encodeFunctionData("requestRandomWords"),
       };
-      console.log("Message prepared for dispatch:", message);
 
-      // Dispatch the message through the Yaho contract on Chiado
-      console.log("Dispatching messages with parameters:", [message], [chiadoAmbAdapterAddress], [goerliAmbAddress]);
+      // Step 1: Dispatch the message
+      console.log("Dispatching message...");
       const dispatchTx = await yahoContract.dispatchMessagesToAdapters(
         [message],
         [chiadoAmbAdapterAddress],
         [goerliAmbAddress],
       );
-      console.log("Dispatch transaction sent:", dispatchTx);
+      await dispatchTx.wait();
+      console.log("Message dispatched.", dispatchTx);
 
-      // Wait for the dispatch transaction to be confirmed
-      const dispatchReceipt = await dispatchTx.wait();
-      console.log("Dispatch transaction confirmed:", dispatchReceipt);
+      // Step 2: Get the signature
+      console.log("Getting signature...");
+      const coder = AbiCoder.defaultAbiCoder();
+      const encodedData = coder.encode(["address", "bytes"], [vrfConsumerAddress, message.data]);
+      console.log("Encoded data:", encodedData);
+      const signature = await ambHelperContract.getSignature(encodedData);
+      console.log("Signature obtained.", signature);
 
-      // Extract the messageId from the event logs of the dispatch transaction receipt
-      const messageId = dispatchReceipt.events.find((event: any) => event.event === "MessageDispatched").args.messageId;
-      console.log("Message ID extracted from event logs:", messageId);
+      // Step 3: Execute the signature
+      console.log("Executing signature...");
+      const executeSignatureTx = await ambContractOnGoerli.executeSignature(encodedData, signature);
+      const executeSignatureReceipt = await executeSignatureTx.wait();
+      console.log("Signature executed.");
 
-      // Execute the message on the Goerli chain using the Yaru contract
+      // Step 4: Extract the messageId from the event logs
+      console.log("Extracting messageId...");
+      const messageId = executeSignatureReceipt.events.find((event: any) => event.event === "MessageDispatched").args
+        .messageId;
+      console.log("MessageId extracted: ", messageId);
+
+      // Step 5: Execute the message
+      console.log("Executing message...");
       const executeTx = await yaruContract.executeMessages(
         [message],
         [messageId],
         [signer.address],
         [goerliAmbAddress],
       );
-      console.log("Execute transaction sent:", executeTx);
+      await executeTx.wait();
+      console.log("Message executed.");
 
-      // Wait for the execute transaction to be confirmed
-      const executeReceipt = await executeTx.wait();
-      console.log("Execute transaction confirmed:", executeReceipt);
-
-      // Start listening for the VRF response after executing the message
+      // Step 6: Listen for the VRF response
+      console.log("Listening for VRF response...");
       listenForVRFResponse(messageId);
+      console.log("Listening for VRF response started.");
 
-      // Update the requestId state for display purposes
       setRequestId(messageId);
       console.log("Request ID set to state:", messageId);
     } catch (error) {
-      // Log any errors that occur during the process
       console.error("Error getting VRF:", error);
       alert("There was an error fetching the VRF.");
     } finally {
-      // Set loading state to false to indicate the process has ended
       setLoading(false);
       console.log("Set loading to false");
     }
@@ -138,7 +146,9 @@ const GetVRF = () => {
         description="From Gnosis's Testnet Chiado chain 🟢✨, you request a Chainlink VRF and listen for the response on Goerli chain."
       />
       <div className="flex flex-col gap-y-6 lg:gap-y-8 py-8 lg:py-12 justify-center items-center">
-        <p className="text-3xl mt-14">Get VRF</p>
+        <p className="text-3xl mt-14"> Use Hashi to request a Chainlink VRF from Goerli on Chiado </p>
+        <p className="text-3m mt-2"> Open the Chrome console to see the magic ! </p>
+        <p className="text-3xl mt-7">Tap for a VRF</p>
         <button
           className={`btn btn-secondary btn-lg font-thin ${loading ? "loading" : ""} bg-base-100`}
           onClick={getVRF}
